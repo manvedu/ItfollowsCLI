8c64815cd096593f10ef0b6c494e8f57e05ea1a8
manvedu <mavelandiad@gmail.com>
2016-01-06 12:30:08 -0500

    chandes to CLI thor

--- to_do-cli.rb
+++ to_do-cli.rb
@@ -7,10 +7,27 @@ require "thor"
 
 class ItfollowsCLI < Thor
   no_commands do
-    def headers
+    def touch(data)
+      f = File.open("algo", "w")
+      f.puts(data)
+      f.close
+    end
+    def output(file_name)
+      contents = File.read(file_name)
+      JSON.parse(contents)
+    end
+
+    def login_headers
       {
-        'X-User-Token' => token,
-        'X-User-Email' => email,
+        'Accept' => 'application/json',
+        'Content-Type' => 'application/json'
+      }
+    end
+
+    def headers(file_name)
+      {
+        'X-User-Token' => token(file_name),
+        'X-User-Email' => email(file_name),
         'Accept' => 'application/json',
         'Content-Type' => 'application/json'
       }
@@ -25,23 +42,23 @@ class ItfollowsCLI < Thor
     def build_http(name, uri)
       Net::HTTP.new(uri.host, uri.port)
     end
-    def get(name, uri)
-      build_http(name, uri).get(uri.path, headers)
+    def get(name, uri, file_name)
+      build_http(name, uri).get(uri.path, headers(file_name))
     end
   
-    def post(name, payload, uri)
-      build_http(name, uri).post(uri.path, payload.to_json, headers)
+    def post(name, payload, uri, file_name)
+      build_http(name, uri).post(uri.path, payload.to_json, headers(file_name))
     end
   
     def host
       'http://localhost:3000'
     end
   
-    def email
-      'mariavelandia@fluvip.com'
+    def email(file_name)
+      output(file_name).values.first
     end
-    def token
-      '6i6cv3zLL5aKfX1zEhNS'
+    def token(file_name)
+      output(file_name).values.last
     end
   end
 
@@ -49,20 +66,20 @@ class ItfollowsCLI < Thor
     method_option :email, required: true
     method_option :password, required: true
   def login
-    headers1 = { 'Accept' => 'application/json', 'Content-Type' => 'application/json' }
     uri = URI.parse("#{host}/users/sign_in.json")
     http = Net::HTTP.new(uri.host, uri.port)
     payload = {"user" => {"email" => options[:email], "password" => options[:password]}} 
-    response = http.post(uri.path,payload.to_json, headers1)
+    response = http.post(uri.path, payload.to_json, login_headers)
     lines = JSON.parse(response.body)
-    #lines.values[0]
+    touch(response.body)
     puts lines
   end
     
   desc "",""
   def list(name)
     uri = uri(name)
-    response = get(name, uri)
+    file_name = "algo"
+    response = get(name, uri, file_name)
     lines = JSON.parse(response.body)
     puts lines.inspect
   end
@@ -70,9 +87,11 @@ class ItfollowsCLI < Thor
   desc "", ""
     method_option :dfn, required: true
   def new(name)
+#ojo porque aca solo esta pensado para un line entry con esa data
     payload = {'line_entry' => {"data" => {"dfn" => options[:dfn]}}}
     uri = uri(name)
-    response = post(name, payload, uri)
+    file_name = "algo"
+    response = post(name, payload, uri, file_name)
     line = JSON.parse(response.body)
     puts "#{line["id"]} #{line["user_id"]} #{line["name"]}"
   end
@@ -81,10 +100,12 @@ class ItfollowsCLI < Thor
     method_option :id, required: true
   def show(name)
     uri = uri_for_show(name, options[:id])
-    response = get(name, uri)
+    file_name = "algo"
+    response = get(name, uri, file_name)
     lines = JSON.parse(response.body)
     puts lines.inspect
   end
+
 end
 
 ItfollowsCLI.start(ARGV)
